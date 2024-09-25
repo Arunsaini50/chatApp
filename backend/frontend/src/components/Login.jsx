72a7d482719980befb645ad756fdc3e83249cead
Arun saini <139784010+Arunsaini50@users.noreply.github.com>
2024-09-25 18:59:14 +0530

    Update Login.jsx

--- backend/frontend/src/components/Login.jsx
+++ backend/frontend/src/components/Login.jsx
@@ -37,7 +37,7 @@ function Login() {
   };
   return (
     <>
-      <div className="flex h-screen items-center justify-center">
+      <div className="flex h-screen items-center justify-center border-black rounded-md">
         <form
           onSubmit={handleSubmit(onSubmit)}
           className="border border-white px-6 py-2 rounded-md space-y-3 w-96"
